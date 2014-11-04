9cb1f11d41c116015668082fb96e788dab0c35b7
ekapop- ubuntu 14.04 <libertywing002@gmail.com>
2014-11-05 01:08:51 +0700

    easy read

--- src/GetNeigbor.java
+++ src/GetNeigbor.java
@@ -26,16 +26,20 @@ public class GetNeigbor {
 		return sb.toString();
 	}
 
-	public static JSONObject readJsonFromUrl(String url) throws IOException,JSONException {
-		InputStream is = new URL(url).openStream();
-		try {
+	public static JSONObject readJsonFromUrl(String ipv4) throws IOException,JSONException {
+		InputStream is = null;
+		try{
+			is = new URL(ipv4).openStream();
 			BufferedReader rd = new BufferedReader(new InputStreamReader(is,
 					Charset.forName("UTF-8")));
 			String jsonText = readAll(rd);
 			JSONObject json = new JSONObject(jsonText);
-			return json;
-		} finally {
 			is.close();
+			return json;
+		}catch(Exception e){
+			String piID = findPiID(ipv4);
+			System.out.println("can't connect to Pi" + piID.substring(0, piID.indexOf(":")) + " : " + ipv4);
+			return null;
 		}
 	}
 
@@ -44,6 +48,7 @@ public class GetNeigbor {
 		try {
 			JSONObject json = readJsonFromUrl("http://" + ipv4 + ":9090");
 			// System.out.println("json = " + json.toString());
+			if(json == null) return;
 			JSONArray neighbors = (JSONArray) json.get("neighbors");
 			// System.out.println("neighbors = " + neighbors);
 
@@ -57,13 +62,12 @@ public class GetNeigbor {
 			e.printStackTrace();
 		}
 		
-		System.out.println("Pi : " + ipv4);
+		System.out.println("Pi" + findPiID(ipv4) + " : " + ipv4);
 		for (int i = 0; i < myArrList.size(); i++){
 			System.out.println(myArrList.get(i));
 		}
 		System.out.println("==========================");
 	}
-
 	
 	public static void getNeighborList(final ArrayList<String> ipv4) {
 		Timer myTimer;
@@ -72,17 +76,27 @@ public class GetNeigbor {
 		myTimer.schedule(new TimerTask() {
 			public void run() {
 				for (int i = 0; i < ipv4.size(); i++) {
-					getNeighbor("192.168.99.1");
+					getNeighbor(ipv4.get(i));
 				}
 			}
 		}, 0, waitingTime);
 		// 1000 = 1 second
 	}
+	
+	public static String findPiID(String ipv4){
+		String piID = ipv4;
+		for(int i=0;i<piID.length();i++){
+			piID = piID.substring(piID.indexOf(".") + 1, piID.length());
+		}
 
+		return piID;
+	}
+	
 	public static void main(String[] args) {
-		ipv4.add("192.168.99.1");
-		ipv4.add("192.168.99.2");
-		ipv4.add("192.168.99.3");
+		ipv4.add("1.1.1.1");
+		ipv4.add("1.1.1.2");
+		ipv4.add("1.1.1.3");
 		getNeighborList(ipv4);
+//		System.out.println(findPiID("1.1.1.2:9090"));
 	}
 }
